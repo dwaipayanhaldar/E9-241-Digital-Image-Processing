9718c220c6a71f6f004c8ffefe28f47971dc7b20
Dwaipayan Haldar <dwai.payan152002@gmail.com>
2025-10-20 13:25:05 +0530

    Assignment 3 upload

---
+++ Assignment_3/src/P01_Directional_Filtering.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def vertical_sin(M):
+    image = np.zeros((M,M))
+    for i in range(M):
+        for j in range(M):
+            image[i,j] = np.sin((2*np.pi*12*i)/M)
+    
+    return image
+
+def horizontal_sin(M):
+    image = np.zeros((M,M))
+    for i in range(M):
+        for j in range(M):
+            image[i,j] = np.sin((2*np.pi*8*j)/M)
+    
+    return image
+
+def diagonal_sin(M):
+    image = np.zeros((M,M))
+    for i in range(M):
+        for j in range(M):
+            image[i,j] = np.sin((2*np.pi*(6*i+10*j))/M)
+    
+    return image 
+
+def directional_filter(dft_img, theta_min, theta_max):
+    M, N = dft_img.shape
+    H_i, H_j = np.meshgrid(np.arange(M), np.arange(N))
+    H_i -= (M//2)
+    H_j -= (N//2)
+    theta = -1*np.arctan2(H_j,H_i)
+
+    theta_min_radian = (theta_min*np.pi)/180
+    theta_max_radian = (theta_max*np.pi)/180
+
+    H = np.where((theta >= theta_min_radian) & (theta<= theta_max_radian), 1,0)
+    return H
+
+def mse(img_ref,img):
+    M, N = img_ref.shape
+    diff_sq = np.abs((img - img_ref)**2)
+    mse = np.sum(diff_sq)/(M*N)
+    return mse
+
+    
---
+++ Assignment_3/src/P02_Gaussian_Blurring_Inverse_Filtering.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gaussian_filter_tf(k = 13, sigma = 2.5):
+    x, y = np.meshgrid(np.arange(-(k//2),k//2+1),np.arange(-(k//2),k//2+1))
+    G_xy = np.exp(-(x**2+y**2)/(2*sigma**2))
+    G_xy /= np.sum(G_xy)
+    return G_xy
+
+
+if __name__ == "__main__":
+    G = gaussian_filter_tf()
+    G_kernel_padded = np.pad(G, ((6,6),(6,6)))
+    plt.imshow(G_kernel_padded, cmap= 'gray')
+    plt.show()
